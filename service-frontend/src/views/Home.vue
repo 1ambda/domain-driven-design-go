1001ebc1921a6006f72f75184da70c76368af4a0
1ambda <1amb4a@gmail.com>
2018-08-15 12:49:31 +0900

    feat: Update main page

--- service-frontend/src/views/Home.vue
+++ service-frontend/src/views/Home.vue
@@ -1,40 +1,35 @@
 <template>
     <div class="home">
-        <img src="../assets/logo.png">
-        <h1>Welcome to Your Vue.js + TypeScript App</h1>
+        <img src="../assets/main.png" width="45%" height="45%" style="margin-top: 50px;">
+        <h1>Domain Driven Design in Go</h1>
         <p>
-            For guide and recipes on how to configure / customize this project,<br>
+            To get or customize this project,<br>
             check out the
-            <a href="https://github.com/vuejs/vue-cli/tree/dev/docs" target="_blank">vue-cli documentation</a>.
+            <a href="https://github.com/1ambda/domain-driven-design-go" target="_blank">repository</a>.
         </p>
-        <h3>Installed CLI Plugins</h3>
+        <h3>Infrastructure Layer</h3>
         <ul>
-            <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a>
-            </li>
-            <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-                   target="_blank">typescript</a></li>
-            <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa"
-                   target="_blank">pwa</a></li>
-            <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest"
-                   target="_blank">unit-jest</a></li>
-            <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-cypress"
-                   target="_blank">e2e-cypress</a></li>
+            <li><a href="https://cloud.google.com/"
+                   target="_blank">GCP</a></li>
+            <li><a href="https://kubernetes.io/"
+                   target="_blank">Kubernetes</a></li>
+            <li><a href="https://istio.io/"
+                   target="_blank">Istio</a></li>
+            <li><a href="https://www.spinnaker.io/"
+                   target="_blank">Spinnaker</a></li>
+            <li><a href="http://terraform.io/"
+                   target="_blank">Terraform</a></li>
+            <li><a href="https://prometheus.io/"
+                   target="_blank">Prometheus</a></li>
         </ul>
-        <h3>Essential Links</h3>
+        <h3>Application Layer</h3>
         <ul>
-            <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-            <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-            <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-            <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-        </ul>
-        <h3>Ecosystem</h3>
-        <ul>
-            <li><a href="https://router.vuejs.org/en/essentials/getting-started.html" target="_blank">vue-router</a>
-            </li>
-            <li><a href="https://vuex.vuejs.org/en/intro.html" target="_blank">vuex</a></li>
-            <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-            <li><a href="https://vue-loader.vuejs.org/en" target="_blank">vue-loader</a></li>
-            <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+            <li><a href="https://github.com/1ambda/domain-driven-design-go/tree/master/service-gateway/internal/domain"
+                   target="_blank">E-commerce Domain</a></li>
+            <li><a href="https://swagger.io/tools/swagger-codegen/"
+                   target="_blank">Swagger Codegen</a></li>
+            <li><a href=""
+                   target="_blank">Automated CI / CD Pipeline</a></li>
         </ul>
     </div>
 </template>
